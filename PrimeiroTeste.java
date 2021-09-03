df741b5d26548905099459094489580b3428922c
unknown <jefson.oliveira@al.infnet.edu.br>
2021-09-02 22:13:08 -0300

    Aula02

--- PrimeiroTeste.java
+++ PrimeiroTeste.java
@@ -3,18 +3,26 @@ public class PrimeiroTeste {
 	public static void main (String[] args){
 		System.out.println("Wello, Word");
 
-		if(args.length == 5){
+		if(args.length == 6){
 		String nome = args[0];
 		String sobrenome = args[1];
 		int idade = Integer.valueOf(args[2]);
 		float salario = Float.valueOf(args[3]);
 		boolean java = Boolean.valueOf(args[4]);
+		int qtdeMes = Integer.valueOf(args[5]);
 
-		System.out.println(nome);
-		System.out.println(sobrenome);
-		System.out.println(idade);
-		System.out.println(salario);
-		System.out.println(java);
+
+		int anoNascimento = 2021 - idade; 
+		float salarioTotal = salario * qtdeMes;
+
+
+		System.out.println("Nome: " + nome);
+		System.out.println("Sobrenome: " + sobrenome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Ano de Nascimento: " + anoNascimento);
+		System.out.println("Salario Mensal: " + salario);
+		System.out.println("Recebimentos: " + salarioTotal);
+		System.out.println("Devjava: " + java);
 		} else {
 		System.out.println("Deu Ruim");
 		}
